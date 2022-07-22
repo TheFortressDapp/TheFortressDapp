18901455b8a3ea97945c9117d42787a192fa721e
Mercedes Jimenez <DMercedes@brasfieldgorrie.com>
2022-07-22 14:17:36 -0400

    Updates

---
+++ precache-manifest.63a7876f9061491c789c817c090a3edb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4b339627a05e302adb2",
+    "url": "/thefortressdapp/static/css/main.4877bfa0.chunk.css"
+  },
+  {
+    "revision": "e4b339627a05e302adb2",
+    "url": "/thefortressdapp/static/js/main.e4b33962.chunk.js"
+  },
+  {
+    "revision": "62da8b1a066eca508f46",
+    "url": "/thefortressdapp/static/css/1.201c54ba.chunk.css"
+  },
+  {
+    "revision": "62da8b1a066eca508f46",
+    "url": "/thefortressdapp/static/js/1.62da8b1a.chunk.js"
+  },
+  {
+    "revision": "f26d3762e83c87a1d0be",
+    "url": "/thefortressdapp/static/js/runtime~main.f26d3762.js"
+  },
+  {
+    "revision": "ffe29aaf23709b0acf5975707a1d158d",
+    "url": "/thefortressdapp/static/media/The_Fortress_Logo_Clear.ffe29aaf.png"
+  },
+  {
+    "revision": "047463106102f0c94914dbf295ea87d6",
+    "url": "/thefortressdapp/static/media/KillaBears.04746310.png"
+  },
+  {
+    "revision": "24e6d3dfbae8f8bf624772dc53b1dff5",
+    "url": "/thefortressdapp/static/media/Women_From_Venus.24e6d3df.jpg"
+  },
+  {
+    "revision": "d98e83ef7f3f0103031053315ceefaeb",
+    "url": "/thefortressdapp/static/media/SmallBros.d98e83ef.png"
+  },
+  {
+    "revision": "3386627c646bda39ba45156c14821bc5",
+    "url": "/thefortressdapp/static/media/Evil_Ape_Club.3386627c.png"
+  },
+  {
+    "revision": "7f05ca4b808f5a797bc14939007d8709",
+    "url": "/thefortressdapp/static/media/MiniSupers.7f05ca4b.png"
+  },
+  {
+    "revision": "7aab7178e41bf57e03d9c69d502d67ee",
+    "url": "/thefortressdapp/index.html"
+  }
+];
